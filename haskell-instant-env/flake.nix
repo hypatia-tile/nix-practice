7180966bdb8f52d3cba71134a5f9cb4804d3bf4c
shinokun <hypatia.tile02021@gmail.com>
2026-01-18 16:57:00 +0900

    Update flake.nix for Haskell instant env
    - ChatGPT recommended changes to flake.nix for better package management and environment setup.

--- haskell-instant-env/flake.nix
+++ haskell-instant-env/flake.nix
@@ -1,36 +1,77 @@
 {
-  description = "Haskell Instant Environment";
+  description = "Instant Haskell dev env (research + CP)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-  flake-utils.lib.eachDefaultSystem (
-    system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      formatter = nixpkgs.legacyPackages.${system}.nixfmt-tree;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-      devShells.default = pkgs.mkShell {
-        packages = [
-          pkgs.zsh
-          (pkgs.haskell.packages.ghc96.ghcWithPackages (ps: [
-            ps.vector
-            ps.containers
-            ps.bytestring
-          ]))
-          pkgs.haskell.packages.ghc96.cabal-install
-          pkgs.haskell.packages.ghc96.haskell-language-server
+        # Pick a compiler once. Change if you want.
+        hp = pkgs.haskell.packages.ghc910; # e.g. ghc96, ghc98, ghc910, etc.
+
+        commonTools = [
+          hp.ghc
+          hp.cabal-install
+          hp.haskell-language-server
+          hp.ghcid
+          hp.ormolu
+          hp.hlint
+
+          # Helpful general tools
+          pkgs.git
+          pkgs.gnumake
         ];
-      };
-    }
-  );
+
+        cpLibs = [
+          # Common CP libraries (available in the environment for repl / scripts)
+          hp.bytestring
+          hp.text
+          hp.containers
+          hp.vector
+          hp.primitive
+          hp.unordered-containers
+          hp.attoparsec
+        ];
+      in
+      {
+        devShells = {
+          # For language feature experiments / theory playground.
+          research = pkgs.mkShell {
+            name = "hs-research";
+            packages = commonTools ++ [
+              # Optional extras for profiling / debugging / docs style work
+              pkgs.time
+              pkgs.hyperfine
+              pkgs.which
+            ];
+
+            shellHook = ''
+              echo "Haskell research shell: ghc=$(ghc --numeric-version), cabal=$(cabal --numeric-version)"
+              echo "Try:  cabal repl  |  ghci  |  ghcid --command 'cabal repl'"
+            '';
+          };
+
+          # Competitive programming oriented.
+          cp = pkgs.mkShell {
+            name = "hs-cp";
+            packages = commonTools ++ cpLibs ++ [
+              pkgs.python3 # sometimes useful for generators/checkers
+            ];
+
+            shellHook = ''
+              echo "Haskell CP shell: ghc=$(ghc --numeric-version)"
+              echo "Suggested flags: -O2 -Wall -Wno-unused-do-bind -rtsopts"
+              echo "Try: ghc -O2 Main.hs && ./Main < input.txt"
+            '';
+          };
+
+          # Default shell if you do `nix develop`
+          default = self.devShells.${system}.research;
+        };
+      });
 }
